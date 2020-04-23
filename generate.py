261bcbcf57b0cf3fb5702226fbce3c1b085f2789
Nick Young <nick.young@auckland.ac.nz>
2020-04-23 16:21:10 +1200

    add a progress bar, generate an audio track

--- generate.py
+++ generate.py
@@ -7,8 +7,11 @@ from cairosvg import svg2png
 import os
 import io
 from PIL import Image
+from tqdm.auto import tqdm # Progress bar
+from pydub import AudioSegment
 
 images = dict([(os.path.splitext(f)[0], Image.open(f"images/{f}", 'r')) for f in os.listdir("images")]) # Open each image from the images folder
+audio_clips = dict([(os.path.splitext(f)[0], AudioSegment.from_file(f"sound clips/{f}")[:1000]) for f in os.listdir("sound clips")])
 
 with open("pgn/kasparov-deep-blue-1997.pgn") as f:
     first_game = chess.pgn.read_game(f)
@@ -30,10 +33,18 @@ board = first_game.board()
 svg = chess.svg.board(board=board)
 render_image("render/0.jpg", svg)
 
-for i, move in enumerate(first_game.mainline_moves()):
+audio = AudioSegment.silent(duration=1000)
+
+moves = list(first_game.mainline_moves())
+for i, move in enumerate(tqdm(moves)):
     board.push(move)
     if board.is_game_over():
         print(board.result())
     piece_type = chess.piece_name(board.piece_type_at(move.to_square))
     svg = chess.svg.board(board=board, lastmove=move, size=400)
     render_image(f"render/{i + 1}.jpg", svg, images[piece_type], board.is_check())
+    clip = audio_clips[piece_type]
+    if board.is_check():
+        clip = clip.overlay(audio_clips["alert"])
+    audio += clip
+audio.export("audio.mp3", format="mp3")
