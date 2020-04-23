b1845854f5caf0f9ba4b70d07e121431e2328735
Nick Young <nick.young@auckland.ac.nz>
2020-04-23 16:26:42 +1200

    tweak ffmpeg command

--- render.sh
+++ render.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -framerate 1 -i render/%d.jpg -i audio.mp3 -c:a copy -shortest -c:v libx264 -pix_fmt yuv420p -r 25 test.mp4
+ffmpeg -framerate 1 -i render/%d.jpg -i audio.mp3 -c:v libx264 -pix_fmt yuv420p -r 25 test.mp4
